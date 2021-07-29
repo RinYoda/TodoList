619a2cfb6dd824f0c77139b3a013a785e7e33706
Rin <>
2021-07-29 23:51:19 +0900

    TodoList

---
+++ todo/forms.py
@@ -0,0 +1,61 @@
+
+
+
+from django import forms
+from .models import Todo
+
+class TodoForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        exclude = ('created_at','updated_at',)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
